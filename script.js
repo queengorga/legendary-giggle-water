8b5c1c204ac2f413f6fbb1a7e73e341376011f9b
Victoria Allen <vickmae.allen@gmail.com>
2018-12-07 15:35:19 -0800

    Added previous and next buttons; Added auto setTimeout to slider

--- script.js
+++ script.js
@@ -1,54 +1,48 @@
-/*var slideIndex = 1;
-showDivs(slideIndex);
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
+var slideIndex = 0;
+showSlides();
+var slides, dots;
 
-function currentDiv(n) {
-    showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
+function showSlides() {
     var i;
-    var x = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    if (n > x.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = x.length }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    slides = document.getElementsByClassName("mySlides");
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" w3-white", "");
+        dots[i].className = dots[i].className.replace(" active", "");
     }
-    x[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " w3-white";
-}*/
-
-//Slideshow
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 8000); 
 }
 
-function showSlides(n) {
-    var i;
-    var pslides = document.getElementsByClassName("slides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > pslides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = pslides.length
-    }
-    for (i = 0; i < pslides.length; i++) {
-        pslides[i].style.display = "none";
+function plusSlides(position) {
+    slideIndex += position;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    else if (slideIndex < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    pslides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
+}
+
+function currentSlide(index) {
+    if (index > slides.length) { index = 1 }
+    else if (index < 1) { index = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[index - 1].style.display = "block";
+    dots[index - 1].className += " active";
 }
