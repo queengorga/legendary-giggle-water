b580b33d844b6b321faa5272d3d7003c2abab34b
Victoria Allen <vickmae.allen@gmail.com>
2018-12-08 03:04:52 -0800

    Changed class name dot to diamond

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 
 var slideIndex = 0;
 showSlides();
-var slides, dots;
+var slides, diamonds;
 
 function showSlides() {
     var i;
     slides = document.getElementsByClassName("mySlides");
-    dots = document.getElementsByClassName("dot");
+    diamonds = document.getElementsByClassName("diamond");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < diamonds.length; i++) {
+        diamonds[i].className = diamonds[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    diamonds[slideIndex - 1].className += " active";
     setTimeout(showSlides, 8000); 
 }
 
@@ -27,11 +27,11 @@ function plusSlides(position) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < diamonds.length; i++) {
+        diamonds[i].className = diamonds[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    diamonds[slideIndex - 1].className += " active";
 }
 
 function currentSlide(index) {
@@ -40,9 +40,9 @@ function currentSlide(index) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < diamonds.length; i++) {
+        diamonds[i].className = diamonds[i].className.replace(" active", "");
     }
     slides[index - 1].style.display = "block";
-    dots[index - 1].className += " active";
+    diamonds[index - 1].className += " active";
 }
